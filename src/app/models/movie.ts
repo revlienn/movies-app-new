3f366a2d541a2b94ad5606c0b6eb8b5a6ef07cef
Revlien Nathalia <revlien.nathalia@expleogroup.com>
2022-10-27 12:27:17 +0100

    movie model done, slide data from API

--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -1,16 +1,16 @@
 export interface Movie {
     adult: false;
-    backdrop_path: '/d6MhreFdMHONqX3iZlJGCF8UkIt.jpg';
-    genre_ids: [28, 12, 14];
-    id: 436270;
-    original_language: 'en';
-    original_title: 'Black Adam';
-    overview: 'Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.';
-    popularity: 4416.78;
-    poster_path: '/3zXceNTtyj5FLjwQXuPvLYK5YYL.jpg';
-    release_date: '2022-10-21';
-    title: 'Black Adam';
-    video: false;
-    vote_average: 7.1;
-    vote_count: 501;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
 }
